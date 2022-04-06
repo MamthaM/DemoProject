4782454a1c15acf01422c076d83e7c956018bd37
Mamatha Mahadevappa <Mahadevappa@EMTEC.local>
2022-04-06 17:42:44 +0200

    Update message

--- HelloWorld/src/main/java/demo/DemoHelloWorld.java
+++ HelloWorld/src/main/java/demo/DemoHelloWorld.java
@@ -2,7 +2,7 @@ package demo;
 public class DemoHelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("First Git Demo");
+		System.out.println("First Git Demo using Jenkins");
 
 	}
 
